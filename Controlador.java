e192feefc935da8433888279f88e909e03d4be2e
Franco Ruiz <francoruiz05g@gmail.com>
2025-06-26 19:45:15 -0300

    Implemetación de la clase Controlador con toString

--- Controlador.java
+++ Controlador.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Controlador {
     public static void mostrarXConsola(Mensaje mensaje){
-        System.out.println(mensaje);
+        System.out.println(mensaje.toString());
 
     }
 }
