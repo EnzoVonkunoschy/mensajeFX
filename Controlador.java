54902acc3571af933fb787231357ebfc6c6d6261
Diegof100 <alefuentess1992@gmail.com>
2025-06-27 16:03:53 -0300

    Subi mi intervencion

---
+++ Controlador.java
@@ -0,0 +1,8 @@
+package com.example.mensajefx;
+
+public class Controlador {
+    public static void mostrarXConsola( Mensaje mensaje){
+        System.out.println(mensaje);
+    }
+
+}
--- Mensaje.java
+++ Mensaje.java
@@ -8,19 +8,20 @@ package com.example.mensajefx;
  *
  * @author OFICINA19
  */
+
 public class Mensaje {
     String destinatario;
     String remitente;
     String texto;
-    
+
     public Mensaje(String des, String rem, String tex){
         this.destinatario = des;
         this.remitente = rem;
         this.texto = tex;
     }
-    
+
     public String toString(){
         return this.destinatario+"  "+this.remitente+"  "+this.texto;
     }
-    
+
 }
