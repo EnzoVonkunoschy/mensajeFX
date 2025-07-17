d3282b4e1ce7016f17d2ce31670de8780d11c1a2
Flavia <flaviaprogramaciontorres@gmail.com>
2025-07-17 19:56:49 -0300

    cambie mi proyecto a mensajeFX

---
+++ Controlador.java
@@ -0,0 +1,7 @@
+package com.example.mensajefx;
+
+public class Controlador {
+    public static void mostrarXConsola(Mensaje mensaje) {
+        System.out.println(mensaje.toString());
+    }
+}
--- Mensaje.java
+++ Mensaje.java
@@ -20,7 +20,7 @@ public class Mensaje {
     }
     
     public String toString(){
-        return this.destinatario+"  "+this.remitente+"  "+this.texto;
+        return "Destinatario: " + this.destinatario+", Remitente: "+this.remitente+", Texto:   "+this.texto;
     }
     
 }
