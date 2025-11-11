05307c65d03dd83e33f07ea20124e5372ae86416
Carla <carlitasalavaguion750@gmail.com>
2025-11-10 21:20:02 -0300

    Cambios del nombre del proyecto

---
+++ App.java
@@ -0,0 +1,24 @@
+package com.example.mensajefx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        MainView vista = new MainView(stage);
+        new MainController(vista);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        Controlador control = new Controlador();
+
+    }
+
+}
